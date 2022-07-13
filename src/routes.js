30aadf0c5ee96a19b15b522e228806be872f6639
CPuPush <fpakpahan44@gmail.com>
2022-07-13 23:20:25 +0700

    first commit

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+const { addBook, getAllBooks } = require('./handler');
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/books',
+        handler: addBook,
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBooks,
+    },
+];
+
+module.exports = routes;
